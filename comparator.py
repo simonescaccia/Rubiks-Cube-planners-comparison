4c76059ec099918ee4ffb956081dd5b367b010f7
Simone <simonescaccia.s@gmail.com>
2024-03-13 11:36:52 +0100

    Update commands comparator.py

--- comparator.py
+++ comparator.py
@@ -2,6 +2,7 @@ import subprocess
 
 SCORPION_PATH = "./planner25/scorpion.sif"
 FAST_DOWNWARD_PATH = "./downward/fast-downward.py"
+DOMAIN_RUBIKS_CUBE_PATH = "./domain-rubiks-cube/"
 
 IDA_STAR_ADD_HEURISTIC = "--search 'iterated([astar(add())])'"
 IDA_STAR_HMAX_HEURISTIC = "--search 'iterated([astar(hmax())])'"
@@ -10,4 +11,18 @@ IDA_STAR_FF_HEURISTIC = "--search 'iterated([astar(ff())])'"
 SCORPION_CMD = SCORPION_PATH + " {domain_file} {problem_file} {plan_file}"
 FAST_DOWNWARD_CMD = FAST_DOWNWARD_PATH + " {domain_file} {problem_file} {heuristic}"
 
-print(subprocess.run(["ls", "-l"], capture_output=True))
+DOMAIN_FILE = DOMAIN_RUBIKS_CUBE_PATH + "domain.pddl"
+PROBLEM_FILE = DOMAIN_RUBIKS_CUBE_PATH + "problem1.pddl"
+
+# code: 0 for scorpion, code: 1 for fast_downward
+def get_plan_and_search_time(output: str, code: int):
+    # find "search exit code"
+    # if search exit code == 0
+    # find "search time"
+    # find plan
+    # compare plan with optimal plan
+    print(output)
+
+command = FAST_DOWNWARD_CMD.format(domain_file=DOMAIN_FILE, problem_file=PROBLEM_FILE, heuristic=IDA_STAR_FF_HEURISTIC)
+output = subprocess.run(command.replace("'",'').split(' '), capture_output=True)
+get_plan_and_search_time(output, 1)
